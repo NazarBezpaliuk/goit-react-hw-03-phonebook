5cd4c6da2e83a44fbce1466c6e5dc02659b79083
NazarBezpaliuk <nazardxvx@gmail.com>
2022-11-19 11:55:17 +0200

    1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -42,7 +42,7 @@ export class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            placeholder="Alex"
+            placeholder="Nazar"
             value={name}
             onChange={this.handleInput}
           />
@@ -55,7 +55,7 @@ export class ContactForm extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            placeholder="111-22-33"
+            placeholder="333-333-333"
             value={number}
             onChange={this.handleInput}
           />
